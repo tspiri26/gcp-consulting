b1eb7e133af8181d312280a64453b53eb66c3d7c
teospiri26 <teodor.spiridonescu@gmail.com>
2024-02-28 13:28:12 +0200

    u

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mysql = require("mysql2");
+const mysql = require("mysql");
 const request = require("request");
 const app = express();
 const ejs = require("ejs");
